5c5ab4724d3279aaf2f6bb44c6cb65852fb014e2
Salvador Ribolzi <ribolzisalvador@gmail.com>
2023-10-12 21:12:02 -0300

    FN_Spotify, QK_BOOT, ALT/GUI Swap

--- keyboards/gmmk/pro/rev1/ansi/keymaps/ribolzisalvador/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/ribolzisalvador/keymap.c
@@ -25,6 +25,7 @@ enum custom_keycodes {
     FN_LLOCK,
     FN_TURBO_MOUSE,
 
+    FN_SPOTIFY,
     FN_SLACK,
     FN_VSCODE,
     FN_STEAM,
@@ -60,12 +61,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FN] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM, KC_MSEL, KC_CALC, FN_TELEGRAM, FN_STEAM, FN_SLACK, FN_VSCODE, _______, _______, _______, _______, _______,  _______,
+        _______, KC_MYCM, KC_WHOM, FN_SPOTIFY, KC_CALC, FN_TELEGRAM, FN_STEAM, FN_SLACK, FN_VSCODE, _______, _______, _______, _______, _______,_______,
         _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, DF(_QWERTY), DF(_COLEMAKDH), RESET, _______,
+        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, DF(_QWERTY), DF(_COLEMAKDH), QK_BOOT, _______,
         _______, _______, RGB_VAD, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,          FN_TURBO_MOUSE,   _______,
         CW_TOGG, _______, _______, _______, _______, _______, _______, REDDIT_S, _______, _______, NK_TOGG,                   KC_CAPS, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+        _______, AG_LNRM, AG_LSWP,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
 
     [_EXTRAS] = LAYOUT(
@@ -143,6 +144,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case FN_RCLICK:
             if (record->event.pressed) {
                 tap_code16(S(KC_F10));
+            }
+            break;
+        case FN_SPOTIFY:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_LGUI) SS_DELAY(100) "SPOTIFY" SS_DELAY(100) SS_TAP(X_ENT));
             }
             break;
         case FN_VSCODE:
